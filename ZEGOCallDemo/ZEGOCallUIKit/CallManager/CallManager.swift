edef3056ce913683204543c587b35b85661626ee
席航 <careyxi@zego.im>
2022-04-16 11:23:14 +0800

    🐛: supplement getToken logic.

--- ZEGOCallDemo/ZEGOCallUIKit/CallManager/CallManager.swift
+++ ZEGOCallDemo/ZEGOCallUIKit/CallManager/CallManager.swift
@@ -157,6 +157,7 @@ class CallManager: NSObject, CallManagerInterface {
     ///   - callType: call type
     ///   - presentVC: Whether present the Call page: default true
     func acceptCall(_ userInfo: UserInfo, callType: CallType, presentVC:Bool = true) {
+        currentCallStatus = .calling
         audioTool.stopPlay()
         guard let userID = userInfo.userID else {
             currentCallStatus = .free
@@ -178,6 +179,9 @@ class CallManager: NSObject, CallManagerInterface {
         }
         
         delegate?.getRTCToken({ token in
+            if self.currentCallStatus != .calling {
+                return
+            }
             guard let token = token else {
                 self.currentCallStatus = .free
                 self.currentCallVC?.changeCallStatusText(.decline)
